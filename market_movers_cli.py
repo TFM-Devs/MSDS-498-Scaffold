ed1c6150fc4ed07d0221fe733a4190269cadac65
Thomas Martin <tfmartin694@gmail.com>
2021-05-14 20:25:36 +0000

    update cli

--- market_movers_cli.py
+++ market_movers_cli.py
@@ -1,9 +1,7 @@
 import boto3
 import click
-from boto3.dynamodb.conditions import Key
 from boto3.dynamodb.conditions import Attr
 import pandas as pd
-import json
 
 Columns = ['PrimaryKey', 
 'Card', 
@@ -22,14 +20,14 @@ Columns = ['PrimaryKey',
 @click.option('--player', prompt="I need the player name", help="Need primary_key")
 
 def player_metric(player):
-   dynamodb = boto3.resource('dynamodb')
-   table = dynamodb.Table('MarketMoversFromS3')
-   response = table.scan(
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('MarketMoversFromS3')
+    response = table.scan(
        FilterExpression=Attr('Player').eq(player))
    
-   df = pd.DataFrame(response['Items'])
-   df = df[['Player','Card','Start_Avg','End_Avg','Percent_Change','Sales_Volume']]
-   print(df)
+    df = pd.DataFrame(response['Items'])
+    df = df[['Player','Card','Start_Avg','End_Avg','Percent_Change','Sales_Volume']]
+    print(df)
 
 if __name__ == '__main__':
     player_metric()
